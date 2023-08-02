b1357dd12b639c267a896c7d306a2ceb8c5e95fe
Luka Erkapic <erkapic.luka.os@gmail.com>
2023-08-02 22:25:06 +0200

    webgl part9

---
+++ WebGL/space_shooter_game/part9/src/content.ts
@@ -0,0 +1,44 @@
+import { Rect } from "./rect";
+import { Sprite } from "./sprite";
+import { Texture } from "./texture";
+
+export class Content 
+{
+    private static spriteSheet: Texture;
+
+    public static sprites: { [id:string] : Sprite }={};
+    public static testUvTexture: Texture;
+
+    public static async initialize(gl: WebGL2RenderingContext)
+    {
+        this.spriteSheet = await Texture.loadTexture(gl, "assets/Spritesheet/sheet.png");
+        this.testUvTexture = await Texture.loadTexture(gl, "assets/test_uv.jpg");
+
+        await this.loadSpriteSheet(); 
+    }
+
+    private static async loadSpriteSheet() 
+    {
+        const sheetXmlReq = await fetch("assets/Spritesheet/sheet.xml");
+        const sheetXmlText = await  sheetXmlReq.text();
+
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(sheetXmlText, "text/xml");
+
+        xmlDoc.querySelectorAll("SubTexture").forEach((subTexture) => {
+
+            const name = subTexture.getAttribute("name")!.replace(".png", "");
+            const x = parseInt(subTexture.getAttribute("x")!);
+            const y = parseInt(subTexture.getAttribute("y")!);
+            const width = parseInt(subTexture.getAttribute("width")!);
+            const height = parseInt(subTexture.getAttribute("height")!);
+
+            const drawRect = new Rect(0, 0, width, height);
+            const sourceRect = new Rect(x, y, width, height);
+
+
+            this.sprites[name] = new Sprite(this.spriteSheet, drawRect, sourceRect);
+        });
+
+    }
+}
---
+++ WebGL/space_shooter_game/part9/src/engine.ts
@@ -0,0 +1,74 @@
+
+import { Color } from "./color";
+import { Content } from "./content";
+import { Rect } from "./rect";
+import { SpriteRenderer } from "./sprite-renderer";
+
+export class Engine {
+    private canvas!: HTMLCanvasElement;
+    private gl!: WebGL2RenderingContext;
+
+    private spriteRenderer!: SpriteRenderer;
+
+
+    constructor() {
+
+    }
+
+    public async initialize() {
+        this.canvas = document.getElementById("canvas") as HTMLCanvasElement;
+        this.gl = this.canvas.getContext("webgl2", {
+            alpha: false,
+        })!;
+        this.gl.pixelStorei(this.gl.UNPACK_FLIP_Y_WEBGL, true);
+
+        await Content.initialize(this.gl);
+
+        this.spriteRenderer = new SpriteRenderer(this.gl, this.canvas.width, this.canvas.height);
+        await this.spriteRenderer.initialize();
+
+    }
+
+    public draw(): void {
+
+        this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+        this.gl.clearColor(0.8, 0.8, 0.8, 1);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+
+        this.spriteRenderer.begin();
+
+        const playerSprite =  Content.sprites["playerShip1_blue"];
+
+        playerSprite.drawRect.x += 1;
+        playerSprite.drawRect.y += 1;
+
+        this.spriteRenderer.drawSpriteSource(
+            playerSprite.texture,
+            playerSprite.drawRect,
+            playerSprite.sourceRect);
+
+        const shieldSprite = Content.sprites["shield3"];
+
+        shieldSprite.drawRect.x = playerSprite.drawRect.x - 22;
+        shieldSprite.drawRect.y = playerSprite.drawRect.y - 25;
+
+        this.spriteRenderer.drawSpriteSource(
+            shieldSprite.texture,
+            shieldSprite.drawRect,
+            shieldSprite.sourceRect,
+            new Color(0,1,1));
+
+        const size = 742 / 2;
+        this.spriteRenderer.drawSpriteSource(
+            Content.testUvTexture,
+            new Rect(0, 0, 100, 100),
+            new Rect(0, size, size, size));
+
+     
+        this.spriteRenderer.end();
+
+        // start game loop 
+        window.requestAnimationFrame(() => this.draw());
+    }
+
+}
